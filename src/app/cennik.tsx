0d35af9dc14157580621538e210bb77f63bdbba7
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-08-07 17:24:33 +0200

    api

---
+++ src/app/cennik.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+//import { useEffect, useState } from "react";
+
+const baseURL =
+  "http://localhost/school1//wp-json/wp/v2/wyjazd_zimowy?acf_format=standard";
+
+export default function Cennik() {
+  //
+  //   useEffect(() => {
+  //     async function getInfo() {
+  //       await axios
+  //         .get(baseURL)
+  //         .then((response) => {
+  //           setPost(response.data);
+  //         })
+  //         .catch(function (error) {
+  //           // handle error
+  //           console.log(error);
+  //         })
+  //         .then(function () {
+  //           // always executed
+  //         });
+  //     }
+  //     getInfo();
+  //   }, []);
+
+  return (
+    <>
+      <p>test</p>
+    </>
+  );
+}
