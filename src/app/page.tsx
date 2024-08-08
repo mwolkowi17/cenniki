39be28820909bbe40f3de06bd429e51e9d32fdef
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-08-08 15:51:01 +0200

    first list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Cennik from "./cennik";
-import { useEffect, useState } from "react";
+import { Key, useEffect, useState } from "react";
 import axios from "axios";
 
 const baseURL = "https://despensapolaca.eu/wp-json/wp/v2/cenniki";
 
 export default function Home() {
-  const [post, setPost] = useState([]);
+  const [post, setPost] = useState<any>([]);
 
   useEffect(() => {
     async function getInfo() {
@@ -29,9 +29,18 @@ export default function Home() {
     getInfo();
   }, []);
   console.log(post);
+
+  const listItems = post.map(
+    (product: { id: Key | null | undefined; acf: any }) => (
+      <li key={product.id}>{<p>{product?.acf.cena_netto_agrosik}</p>}</li>
+    )
+  );
+  if (!post) return null;
+
   return (
     <main className={styles.main}>
-      <Cennik />
+      <Cennik number={post[0]?.acf.cena_netto_agrosik} />
+      <ul>{listItems}</ul>
     </main>
   );
 }
